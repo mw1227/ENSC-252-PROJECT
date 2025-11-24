67660360666d66d261c6ba736273abe328dc5263
mw1227 <matthewlhwong@gmail.com>
2025-11-23 22:32:34 -0800

    rewrote display_driver, now recieves states, led signals and hex signals

--- display_driver.vhd
+++ display_driver.vhd
@@ -1,71 +1,57 @@
-library ieee;
-use ieee.std_logic_1164.all;
-use ieee.numeric_std.all;
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
 
 entity display_driver is
-    port (
-        clk       : in  std_logic;
-        reset     : in  std_logic;
-        floor_in  : in  integer range 0 to 7;
-        state     : in  integer range 0 to 6;
-        hex_floor : out std_logic_vector(6 downto 0);
-        hex_door  : out std_logic_vector(6 downto 0);
-        estop_led : out std_logic
+    Port (
+        floor_in   : in  STD_LOGIC_VECTOR(2 downto 0);
+        state_in   : in  STD_LOGIC_VECTOR(2 downto 0);
+        led_dir    : out STD_LOGIC_VECTOR(2 downto 0);
+        led_estop  : out STD_LOGIC;
+        hex_floor  : out STD_LOGIC_VECTOR(6 downto 0);
+        hex_door   : out STD_LOGIC_VECTOR(6 downto 0)
     );
-end entity;
+end display_driver;
 
 architecture Behavioral of display_driver is
-
-    type state_t is (
-        IDLE, MOVING_UP, MOVING_DOWN,
-        SERVING, ESTOP, DOOR_OPEN, DOOR_CLOSE
-    );
-
-    signal s_state : state_t;
-
 begin
 
-    with state select
-        s_state <=
-            IDLE        when 0,
-            MOVING_UP   when 1,
-            MOVING_DOWN when 2,
-            SERVING     when 3,
-            ESTOP       when 4,
-            DOOR_OPEN   when 5,
-            DOOR_CLOSE  when 6,
-            IDLE        when others;
-
-    process(s_state, floor_in)
+    process(state_in)
     begin
-        hex_floor <= "1111111";
-        hex_door  <= "1111111";
-        estop_led <= '0';
+        led_estop <= '0';
+        led_dir   <= "100";
+        hex_door  <= "1000110";
 
-        case floor_in is
-            when 0 => hex_floor <= "1111001";
-            when 1 => hex_floor <= "0100100";
-            when 2 => hex_floor <= "0110000";
-            when 3 => hex_floor <= "0011001";
-            when 4 => hex_floor <= "0010010";
-            when 5 => hex_floor <= "0000010";
-            when 6 => hex_floor <= "1111000";
-            when 7 => hex_floor <= "0000000";
-            when others => hex_floor <= "1111111";
-        end case;
+        case state_in is
+            when "010" =>
+                led_dir <= "010";
 
-        case s_state is
-            when IDLE | MOVING_UP | MOVING_DOWN | DOOR_CLOSE =>
-                hex_door <= "1000110";
+            when "011" =>
+                led_dir <= "001";
 
-            when SERVING | DOOR_OPEN =>
+            when "101" =>
                 hex_door <= "1000000";
 
-            when ESTOP =>
-                estop_led <= '1';
-                hex_floor <= "1111111";
-                hex_door  <= "1111111";
+            when "111" =>
+                led_estop <= '1';
+
+            when others =>
+                null;
         end case;
     end process;
 
-end architecture;
+    process(floor_in)
+    begin
+        case floor_in is
+            when "000" => hex_floor <= "1111001";
+            when "001" => hex_floor <= "0100100";
+            when "010" => hex_floor <= "0110000";
+            when "011" => hex_floor <= "0011001";
+            when "100" => hex_floor <= "0010010";
+            when "101" => hex_floor <= "0000010";
+            when "110" => hex_floor <= "1111000";
+            when "111" => hex_floor <= "0000000";
+            when others => hex_floor <= "1111111";
+        end case;
+    end process;
+
+end Behavioral;
